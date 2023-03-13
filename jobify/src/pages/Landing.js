be9490b4e73e4498b78f2aefbf40788d3e3345ec
wasiii960 <waasif.ali960@gmail.com>
2023-03-13 15:40:07 +0500

    boiler plate code for jobify

---
+++ jobify/src/pages/Landing.js
@@ -0,0 +1,10 @@
+import logo from '../assets/images/logo.svg'
+const Landing = () => {
+  return (
+    <div>
+      <img src={logo} alt="jobify"></img>
+    </div>
+  )
+}
+
+export default Landing
