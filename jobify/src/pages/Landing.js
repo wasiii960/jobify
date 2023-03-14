bfae908db60e58c158a5efd553fc477e2a0e11a7
wasiii960 <waasif.ali960@gmail.com>
2023-03-14 23:35:29 +0500

    Landing Page

--- jobify/src/pages/Landing.js
+++ jobify/src/pages/Landing.js
@@ -1,10 +1,28 @@
-import logo from '../assets/images/logo.svg'
+import logo from "../assets/images/logo.svg";
+import main from "../assets/images/main.svg";
+import Wrapper from '../assets/wrappers/LandingPage';
 const Landing = () => {
   return (
-    <div>
-      <img src={logo} alt="jobify"></img>
-    </div>
-  )
-}
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="jobify" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            I'm baby migas thundercats semiotics godard, cred pug gatekeep 8-bit
+            intelligentsia aesthetic kitsch live-edge. Gluten-free fixie prism
+            semiotics.
+          </p>
+          <button className="btn btn-heri">Login/Register</button>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+      </Wrapper>
+  );
+};
 
-export default Landing
+export default Landing;
